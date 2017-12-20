5b83baa42e8e8257e6a74e4d8330758f1072fd08
krlargo <krlargo@ucdavis.edu>
2017-12-19 19:45:39 -0800

    Start UITests

---
+++ BullsEyeMockTests/BullsEyeMockTests.swift
@@ -0,0 +1,38 @@
+//
+//  BullsEyeMockTests.swift
+//  BullsEyeMockTests
+//
+//  Created by Kevin Largo on 12/19/17.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import BullsEye
+
+class MockUserDefaults: UserDefaults {
+    var gameStyleChanged = 0
+    override func set(_ value: Int, forKey defaultName: String) {
+        if defaultName == "gameStyle" {
+            gameStyleChanged += 1
+        }
+    }
+}
+
+class BullsEyeMockTests: XCTestCase {
+    
+    var controllerUnderTest: ViewController!
+    var mockUserDefaults: MockUserDefaults!
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+}
